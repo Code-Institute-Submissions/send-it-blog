86a49c7f48064f6b36a59a9a0a434392d25caec3
louparker <lupaka91@gmail.com>
2021-02-07 21:19:27 +0000

    Add create post form to create post.html

--- app.py
+++ app.py
@@ -101,6 +101,12 @@ def logout():
     return redirect(url_for("login"))
 
 
+
+@app.route("/create_post")
+def create_post():
+    return render_template("create_post.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
